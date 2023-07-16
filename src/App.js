03731276c5c74d2a97b236878672b37f6237cd74
Jonathancodesurfer <jonathanalpizar43@gmail.com>
2023-07-16 16:52:26 -0600

    added router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import './App.css';
-import Navbar from './components/navbar';
-import Header from './components/header';
-import ItemListContainer from './components/itemListContainer';
+import "./App.css";
+import Navbar from "./components/navbar";
+import Header from "./components/header";
+import ItemListContainer from "./components/itemListContainer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className=" ">
-        <Navbar bgColor='red' />
-        <Header title='Your Trusted Cannabis Resource' subtitle='Discover the World of Cannabis with Expert Reviews, Guides, and More!'/>
-        <ItemListContainer title='Our Premium  Products' subtitle='A Wide Range of High-Quality Strains'/>
-      
+      <BrowserRouter>
+        <Navbar bgColor="red" />
+        <Routes>
+        <Route
+          path="/"
+          element={
+            <Header
+              title="Your Trusted Cannabis Resource"
+              subtitle="Discover the World of Cannabis with Expert Reviews, Guides, and More!"
+            />
+          }
+        />
+        </Routes>
+       
+
+        <ItemListContainer
+          title="Our Premium  Products"
+          subtitle="A Wide Range of High-Quality Strains"
+        />
+      </BrowserRouter>
     </div>
   );
 }
